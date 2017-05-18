04ce522c4df7cbda34073a0f7347e376905f6460
Don Cooper <don_cooper@hotmail.com>
2017-05-18 14:26:08 -0400

    Done Time and File

--- ruby-docs.rb
+++ ruby-docs.rb
@@ -84,15 +84,24 @@
 #       => hash = {"key" => value, "key" => value,}
 #       => hash.has_key?("fred") = false
 #
-# => 
+# Time
+#-----
+# => now
+#   => Arguments
+#       => none
+#   => Return type
+#       => time
+#   => Example
+#       => Time.now = 2017-05-18 2:22
 #
-#
-#
-#
-#
-#
-#
-#
-#
-#
+# File
+#-----
+# => exist?
+#   => Arguments
+#       => file_name
+#   => Return type
+#       => boolean
+#   => Example
+#       => file.txt
+#       => exist?(file.tx) = true
 #
