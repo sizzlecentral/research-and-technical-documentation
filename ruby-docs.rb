3af075a2d0e5651b18577849f116302b85f7f15b
Don Cooper <don_cooper@hotmail.com>
2017-05-18 13:58:49 -0400

    Done string

---
+++ ruby-docs.rb
@@ -0,0 +1,48 @@
+# Ruby Docs Research
+#-------------------
+#
+# String
+#-------
+# => length
+#   =>  Arguments
+#       => none
+#   => Return type
+#       => integer
+#   => Example
+#       => "abc".length = 3
+#
+# => strip
+#   => Arguments
+#       => none
+#   => Return type
+# =>    => string
+#   => Example
+#       => "  abc  ".strip = "abc"
+#
+# => split
+#   => Arguments
+#       => pattern, limit
+#   => Return type
+#       => array
+#   => Example
+#       => "it's a string".split = ["it's", "a", "string"]
+#
+# => start_with?
+#   => Arguments
+#       => prefixes
+#   => Return type
+#       => boolean
+#   => Example
+#       => "abcd".start_with?("hell") = false
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
